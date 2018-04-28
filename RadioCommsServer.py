f103cd04a33e121079a1dd2eea4c988b99d40c5f
HaoYe <haoye94@hotmail.com>
2018-04-28 13:18:07 -0400

    Fixed bugs and added audio receive from driver function
    - fixed broadcasting issue

--- RadioCommsServer.py
+++ RadioCommsServer.py
@@ -5,13 +5,17 @@ import struct
 import RPi.GPIO as GPIO
 import sched, time
 
+# Variables
+socketList = []
+users = dict()
 audioEnabled = False
+driverSpeaking = False
 
 def broadcast_server_IP(addr): 
     while True:
         #print("[+] Sending {0}:{1}" .format(UDP_BROADCAST_ADDR, UDP_BROADCAST_PORT))
         broadcastSocket.sendto((BROADCAST_SERVER_IP_COMMAND).to_bytes(1, byteorder='big') + bytes(addr, 'utf-8'), (UDP_BROADCAST_ADDR, UDP_BROADCAST_PORT))
-        time.sleep(2)
+        time.sleep(5)
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,41 +25,66 @@ def get_ip_address(ifname):
         struct.pack('256s', bytes(ifname[:15], 'utf-8'))
     )[20:24])
 
+def broadcastMessage(messageBytes):
+    global socketList
+    for client in socketList:
+        try:
+            client.send(messageBytes) # grant access message
+        except Exception as e:
+            socketList.remove(client)
+
+def handle_driver_audio(socketList):
+    global driverSpeaking
+    while True:
+        if (not GPIO.input(AUDIO_ON_PIN)) and not driverSpeaking:
+            GPIO.output(AUDIO_PTT_PIN, True);
+            driverSpeaking = True
+            print("[+] AUDIO_ON pin activated")
+            broadcastMessage(AUDIO_RECEIVE_ACTIVE.encode() + "Driver\n".encode())
+        else:
+            driverSpeaking = False
+            #print("[+] AUDIO_ON pin deactivated")
+            broadcastMessage(AUDIO_TERMINATE.encode())
+
+
 def handle_client(clientSocket, addr, socketList):
+    global audioEnabled
+    global users
+    global driverSpeaking
+    print("[+] AUDIO_ON pin activated")
+    
     while clientSocket != None:
         try:
             data = clientSocket.recv(1) # receive 1 byte as the identifier for the type of message
             if data:
                 print(data)
-                global audioEnabled
-                global users
                 # identify the type of request : register user, request audio, terminate audio
                 if data[0] == REGISTER_USER_COMMAND:
                     print("[+] Data Received: Register User")
                     userID = clientSocket.recv(MAX_USER_NAME_LENGTH)
                     users[clientSocket] = userID;
                     print("[+] Registered user: %s" % (userID))
-                    clientSocket.send("Registered\n".encode())
+                    clientSocket.send(REGISTER_USER_SUCCESSFUL.encode())
                 elif data[0] == REQUEST_AUDIO_COMMAND:
                     print("[+] Data Received: Request Audio")
-                    if not audioEnabled:
+                    if not (audioEnabled and driverSpeaking):
                         print("[+] AudioEnabled = True")
                         audioEnabled = True # Acknowledge, broadcast to all users
-                        clientSocket.send(GRANT_AUDIO_ACCESS_COMMAND.encode());
-                        try:
-                            for client in socketList:
-                                client.send(users[clientSocket] + " transmitting\n".encode()) # grant access message
-                        except Exception as e:
-                            print(e.args)
-                        GPIO.output(AUDIO_PTT_PIN, False);    
+                        clientSocket.send(AUDIO_TRANSMIT_ACCEPTED.encode());
+                        for client in socketList:
+                            if not (client is clientSocket):
+                                try:
+                                    client.send(messageBytes) # grant access message
+                                except Exception as e:
+                                    socketList.remove(client)
+                        GPIO.output(AUDIO_PTT_PIN, False);
                     else:
-                        clientSocket.send("Channel in use\n".encode())
+                        clientSocket.send(AUDIO_TRANSMIT_REJECTED.encode())
                 elif data[0] == TERMINATE_AUDIO_COMMAND:
                     print("[+] Data Received: Terminate Audio")
                     audioEnabled = False
                     GPIO.output(AUDIO_PTT_PIN, True)
-                    for client in socketList:
-                        client.send((TERMINATE_AUDIO_COMMAND).to_bytes(1, byteorder='big') + "\n".encode()) # broadcast terminate audio message
+                    broadcastMessage(AUDIO_TERMINATE.encode())
         except Exception as e:
             pass
             #print(e.args)
@@ -63,9 +92,15 @@ def handle_client(clientSocket, addr, socketList):
 # Constants
 REGISTER_USER_COMMAND = 0x7E
 REQUEST_AUDIO_COMMAND = 0x8E
-GRANT_AUDIO_ACCESS_COMMAND = "Granted\n"
 TERMINATE_AUDIO_COMMAND = 0xAE
 BROADCAST_SERVER_IP_COMMAND = 0xFE
+
+REGISTER_USER_SUCCESSFUL = "Registered\n";
+AUDIO_TRANSMIT_ACCEPTED = "Granted\n";
+AUDIO_TRANSMIT_REJECTED = "Channel in use\n";
+AUDIO_RECEIVE_ACTIVE= "Transmit";
+AUDIO_TERMINATE = "Terminate\n";
+
 
 LOCAL_IP = get_ip_address('eth0')
 TCP_PORT = 1880
@@ -75,17 +110,13 @@ UDP_BROADCAST_DELAY = 1
 MAX_NO_OF_CONNECTIONS = 20  
 SOCKET_READ_TIMEOUT = 0.5
 MAX_USER_NAME_LENGTH = 10 # bytes
-AUDIO_ON_PIN = 3
-AUDIO_PTT_PIN = 5
-
-# Variables
-socketList = []
-users = dict()
+AUDIO_ON_PIN = 23
+AUDIO_PTT_PIN = 7
 
 # Initializations
 # GPIO for controller SA818
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(AUDIO_ON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(AUDIO_ON_PIN, GPIO.IN)
 GPIO.setup(AUDIO_PTT_PIN, GPIO.OUT)
 GPIO.output(AUDIO_PTT_PIN, True)
 # TCP socket
@@ -97,9 +128,7 @@ print("[+] Listening on {0}:{1}" .format(LOCAL_IP, TCP_PORT))
 broadcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 broadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 threading.Thread(target=broadcast_server_IP, args=(LOCAL_IP,)).start()
-# broadcast_server_IP_scheduler = sched.scheduler(time.time, time.sleep)
-# broadcast_server_IP_scheduler.enter(UDP_BROADCAST_DELAY, 1, broadcast_server_IP, (LOCAL_IP,))
-# broadcast_server_IP_scheduler.run(blocking=False)
+threading.Thread(target=handle_driver_audio,args=(socketList,)).start()
 
 # Blocking process for listenning to incoming TCP connections
 while True:
